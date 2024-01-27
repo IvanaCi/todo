433fa3e3b94564acf5d69719513b526a48f0c96c
Ivana Cigna <ivana.cigna@gmail.com>
2024-01-27 11:13:18 +0000

    Style to do card

--- resources/views/livewire/to-do-card.blade.php
+++ resources/views/livewire/to-do-card.blade.php
@@ -1,12 +1,12 @@
-<div class="grid grid-cols-2" id='{{$todo->id.'-card'}}'>
+<div class="grid grid-cols-[3fr_1fr] bg-sky-50 m-2 p-4 rounded" id='{{$todo->id.'-card'}}'>
 
     @if($editMode)
-        <div>
+        <div class="grid grid-rows-2">
 
             <div>
                 <input wire:model="description" type="text"/>
                     @error('description') <span class="error">{{ $message }}</span> @enderror
-            <div>
+            </div>
 
             <div>
                 <label for="due-date">Due Date</label>
@@ -14,27 +14,30 @@
                     @error('dueDate') <span class="error">{{ $message }}</span> @enderror
             </div>
         </div>
-        <div>
-            <button wire:click.prevent="update" type="button">Save</button>
-            <button wire:click.prevent="toggleEditMode" type="button">Cancel</button>
+        <div class="grid grid-rows-2  p-4">
+            <button class="text-sm capitalize bg-green-300 p-3 rounded" wire:click.prevent="update" type="button">Save</button>
+            <button class="text-sm capitalize bg-red-600 p-3 rounded" wire:click.prevent="toggleEditMode" type="button">Cancel</button>
         </div>
         
     @else
-        <div>
-
-            <div class="{{$completed ? 'completed' : 'pending'}}"> 
+        
+        <div class="grid grid-rows-2">
+            <div class="flex">
                 <input wire:change='updateCompleted()' type="checkbox" wire:model.live='completed'/>
-                <h3>{{$todo->description}}</h3>
+                <h3 class="ml-3">{{$todo->description}}</h3>
+            </div>
+            <div>
                 @if($todo->due_date)
-                    <p>Due on: <span>{{$todo->due_date}}</span></p>
+                    <p class='italic'>Due on: <span>{{$todo->due_date}}</span></p>
                 @endif
             </div>
-
-            <div>
-                <button type="button" wire:click.prevent="toggleEditMode()">Edit</button>
-                <button type="button" wire:click.prevent="delete()">Delete</button>
-            </div>
         </div>
+
+        <div class="grid grid-rows-2 p-4">
+            <button class="text-sm capitalize bg-blue-600 p-2 m-1 rounded" type="button" wire:click.prevent="toggleEditMode()">Edit</button>
+            <button class="text-sm capitalize bg-red-600 p- m-1 rounded" type="button" wire:click.prevent="delete()">Delete</button>
+        </div>
+
         
     @endif
 
