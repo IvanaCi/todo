bdd098355f9f6da444b3f648541c940556eec85f
Ivana Cigna <ivana.cigna@gmail.com>
2024-01-27 00:14:16 +0000

    User can delete a task

--- app/Livewire/ToDoList.php
+++ app/Livewire/ToDoList.php
@@ -5,16 +5,32 @@ namespace App\Livewire;
 use App\Models\Todo;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Throwable;
 
 class ToDoList extends Component
 {
     public $searchString = "";
 
-    #[On('todo-created')] 
+    #[On('list-changed')] 
     public function render()
     {
         $todos = Todo::where('description', 'like', "%$this->searchString%")->orderBy('is_completed')->orderBy('due_date', 'Desc')->orderBy('created_at', 'Asc')->paginate(10);
         return view('livewire.to-do-list', ['todos' => $todos]);
+    }
+
+    public function delete($id){
+        try{
+            $todo = Todo::find($id);
+
+            $todo->delete();
+
+            $this->render();
+        
+        }catch(Throwable $e){
+            session()->flash('status', 'There was an issue processing your request. Please try again.');
+
+            report($e);
+        }
     }
 
 }
--- resources/views/livewire/includes/to-do-card.blade.php
+++ resources/views/livewire/includes/to-do-card.blade.php
@@ -1,6 +1,19 @@
-<div class="card">
-    <h3>{{$todo->description}}</h3>
-    @if($todo->due_date)
-        <p>Due on: <span>{{$todo->due_date}}</span></p>
-    @endif
+<div class="grid">
+    <div> 
+        <h3>{{$todo->description}}</h3>
+        @if($todo->due_date)
+            <p>Due on: <span>{{$todo->due_date}}</span></p>
+        @endif
+
+        @if(session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
+    </div>
+
+    <div>
+        {{-- <button type="button" wire:click.prevent="startEditing({{$todo->id}})">Edit</button> --}}
+        <button type="button" wire:click.prevent="delete({{$todo->id}})">Delete</button>
+    </div>
 </div>
