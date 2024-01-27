fec30d03fd77ecf51140cbbbd7ab6069fbb4d871
Ivana Cigna <ivana.cigna@gmail.com>
2024-01-27 14:44:48 +0000

    Improve UI

--- resources/views/livewire/to-do-card.blade.php
+++ resources/views/livewire/to-do-card.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-sky-50 m-2 p-4 rounded" id='{{$todo->id.'-card'}}'>
+<div class="bg-sky-50 m-1 p-1 rounded" id='{{$todo->id.'-card'}}'>
 
     @if($editMode)
         @livewire('to-do-form', ['todoId'=>$todo->id])          
@@ -7,24 +7,26 @@
 
         <div class="grid grid-cols-[3fr_1fr]">
             <div class="grid grid-rows-2">
-                <div class="flex">
+                <div class="flex items-center">
                     <input wire:change='updateCompleted()' type="checkbox" wire:model.live='completed'/>
                     <h3 class="ml-3">{{$todo->description}}</h3>
                 </div>
-                <div>
+                <div class="flex items-center">
                     @if($todo->due_date)
                         <p class='italic'>Due on: <span>{{$todo->due_date}}</span></p>
 
                         @if($todo->due_date<now())
                             <div class="bg-red-700 text-white text-xs capitalize float-right p-1 mr-2 rounded">Overdue</div>
                         @endif
+                        <p class='italic'>Due on: <span>{{$todo->due_date}}</span></p>                       
                     @endif
+
                 </div>
             </div>
 
             <div class="grid grid-rows-2 p-4">
-                <button class="text-sm capitalize bg-blue-600 p-2 m-1 rounded" type="button" wire:click.prevent="enterEditMode()">Edit</button>
-                <button class="text-sm capitalize bg-red-600 p- m-1 rounded" type="button" wire:click.prevent="delete()">Delete</button>
+                <button class="text-sm capitalize bg-blue-600 p-1 m-1 rounded" type="button" wire:click.prevent="enterEditMode()">Edit</button>
+                <button class="text-sm capitalize bg-red-600 p-1 m-1 rounded" type="button" wire:click.prevent="delete()">Delete</button>
             </div>
         </div>  
 
--- resources/views/livewire/to-do-list.blade.php
+++ resources/views/livewire/to-do-list.blade.php
@@ -1,18 +1,21 @@
-<div>
+<div class="h-4/5 grid drid-rows-4">
     <div>
+        {{-- Display response statuses --}}
         @if(session('status'))
-        <div class="alert alert-success">
+        <div class="alert">
             {{ session('status') }}
         </div>
+
         @endif
-        <label for="search-string" class="font-semibold">Search:</label>
-        <input wire:model.live='searchString' name='search-string' class="rounded border-solid border-4 border-slate-50 mx-3 p-2"/>
-        @if($addMode)
-            
-            @include('livewire.includes.add-to-do')
-        @else
-            <button class="text-sm capitalize bg-blue-600 p-2 m-1 rounded" type="button" wire:click.prevent='enterAddMode()'>Add Task</button>
-        @endif
+
+        <div>
+            @if($addMode)
+                
+                @include('livewire.includes.add-to-do')
+            @else
+                <button class="text-sm capitalize bg-blue-600 p-2 mx-3 rounded float-right" type="button" wire:click.prevent='enterAddMode()'>Add Task</button>
+            @endif
+        </div>
         
     </div>
 
@@ -25,5 +28,10 @@
 
     </div>
 
+    <div class="flex w-full items-center">
+        <label for="search-string" class="font-semibold">Search:</label>
+        <input wire:model.live='searchString' name='search-string' class="rounded border-solid border-4 border-slate-50 mx-3 p-2 flex-grow"/>
+    </div>
     {{ $todos->links()}}
+    
 </div>
